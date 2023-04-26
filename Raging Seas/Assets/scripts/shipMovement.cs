0cf70b095eb1caa3c15cc018913269d3b783f77c
howardxu23 <xu.howard@hotmail.com>
2023-04-26 22:00:13 +0100

    Revert "Added Pirate Pack"
    This reverts commit 9a35f04c90c6b9bf514a07dd1f802a1804efd726.

---
+++ Raging Seas/Assets/scripts/shipMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shipMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private float rotatonSpeed;
+
+    public int maxHealth = 100;
+    public int health=100;
+    public int repairRate=0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
