bbe627152387236bdcf26570b05d7e0a2ee1e30a
howardxu23 <xu.howard@hotmail.com>
2023-04-27 22:11:58 +0100

    UI framework added

--- Raging Seas/Assets/scripts/UIstats.cs
+++ Raging Seas/Assets/scripts/UIstats.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class UIstats : MonoBehaviour
 {
+    [SerializeField]
+    private TMP_Text health;
+    [SerializeField]
+    private TMP_Text coins;
+    [SerializeField]
+    GameObject playerShip;
     // Start is called before the first frame update
     void Start()
     {
