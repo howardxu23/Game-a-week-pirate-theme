e1287a6888cb3e3fae2c5cd8bbd914a9a5a73bc0
Joseph Warner <josephtoko77@gmail.com>
2023-04-30 16:48:36 +0100

    Added SkillTree Design
    Working on functionality later today

--- Raging Seas/Assets/scripts/SkillTree.cs
+++ Raging Seas/Assets/scripts/SkillTree.cs
@@ -1,38 +1,38 @@
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
-//public class SkillTree : MonoBehaviour
-//{
-//    public static SkillTree skillTree;
-//    private void Awake() => skillTree = this;
-//    public int[] SkillLevels;
-//    public int[] SkillCaps;
-//    I
-//public string[] SkillNames;
-//    public string[] SkillDescriptions;
-//    public List<Skill> SkillList;
-//    public GameObject SkillHolder;
-//    public int SkillPoint;
-//    private void Start()
-//}
-//}
-//SkillPoint = 20;
-//Skill Levels = new int[6];
-
-//public List<Skill> SkillList; public GameObject Skill Holder;
-//public int SkillPoint;
-//private void Start()
-//}
-//SkillPoint = 20;
-//Skill Levels = new int[6];
-//SkillCaps = new[] { 1, 5, 5, 2, 10, 10);
-//SkillNames = new[] { "Upgrade 1", "Upgrade 2", "Upgrade 3", "Upgrade 4", "Booster 5", "Booster 6", }; Skill Descriptions = new[]
-//{
-//"Does a thing",
-//"Does a cool thing",
-//"Does a really cool thing",
-//"Does an awesome thing",
-//"Does this math thing",
-//"Does this compounding thing",
-//};
-//foreach (var skill in Skill Holder.GetComponents InChildren<Skill>()) SkillList.Add(skill);
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// public class SkillTree : MonoBehaviour
+// {
+//     public static SkillTree skillTree;
+//     private void Awake() => skillTree = this;
+//     public int[] SkillLevels;
+//     public int[] SkillCaps;
+//     I
+// public string[] SkillNames;
+//     public string[] SkillDescriptions;
+//     public List<Skill> SkillList;
+//     public GameObject SkillHolder;
+//     public int SkillPoint;
+//     private void Start()
+// }
+// }
+// SkillPoint = 20;
+// Skill Levels = new int[6];
+//
+// public List<Skill> SkillList; public GameObject Skill Holder;
+// public int SkillPoint;
+// private void Start()
+// }
+// SkillPoint = 20;
+// Skill Levels = new int[6];
+// SkillCaps = new[] { 1, 5, 5, 2, 10, 10);
+// SkillNames = new[] { "Upgrade 1", "Upgrade 2", "Upgrade 3", "Upgrade 4", "Booster 5", "Booster 6", }; Skill Descriptions = new[]
+// {
+// "Does a thing",
+// "Does a cool thing",
+// "Does a really cool thing",
+// "Does an awesome thing",
+// "Does this math thing",
+// "Does this compounding thing",
+// };
+// foreach (var skill in Skill Holder.GetComponents InChildren<Skill>()) SkillList.Add(skill);
