7efecdc0fbb2461593b87c0501e2fe628009df67
howardxu23 <xu.howard@hotmail.com>
2023-04-30 01:26:36 +0100

    fitted navmesh to the scence

--- Raging Seas/Assets/scripts/AIscript.cs
+++ Raging Seas/Assets/scripts/AIscript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class AIscript : MonoBehaviour
 {
@@ -16,7 +17,9 @@ public class AIscript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        var agent = GetComponent<NavMeshAgent>();
+        //agent.updateRotation = false;
+        agent.updateUpAxis = false;
     }
 
     // Update is called once per frame
